bba933976ca0748e02bbc51c66384e92cf978297
Bojan Petrusevski <bojan.petrusevski@crowdbotics.com>
2022-12-28 15:27:50 +0100

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "project_abc_37809",
   url: "https://project_abc_37809.botics.co",
   api: "https://project_abc_37809.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
